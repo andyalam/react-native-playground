675a3e02287be14a7d5427b4b2a520979aabef49
andy <andyalam@hotmail.com>
2017-02-06 21:34:12 -0800

    building auth form, firebase setup

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+      firebase.initializeApp({
+        apiKey: "AIzaSyB-3vBILDt91IdKHNSGoiuP_-7_UNQ1iZE",
+        authDomain: "auth-rn-10ee6.firebaseapp.com",
+        databaseURL: "https://auth-rn-10ee6.firebaseio.com",
+        storageBucket: "auth-rn-10ee6.appspot.com",
+        messagingSenderId: "177679243394"
+      });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
