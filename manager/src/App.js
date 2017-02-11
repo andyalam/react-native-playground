33391bd36d96818d8dd06f67643b88b1a471e73c
andy <andyalam@hotmail.com>
2017-02-11 13:23:38 -0800

    common components, firebase setup, ios/android switched to same start point

---
+++ manager/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDthLVzvuhb2sLoLLPkmf5uFV759L99XhU",
+      authDomain: "manager-9f5e3.firebaseapp.com",
+      databaseURL: "https://manager-9f5e3.firebaseio.com",
+      storageBucket: "manager-9f5e3.appspot.com",
+      messagingSenderId: "975725731256"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>test</Text>
+        </View>
+      </Provider>
+    );
+  }
+};
+
+export default App;
