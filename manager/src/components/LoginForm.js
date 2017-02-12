12aab5ecd727b78f2a1836af55d6b451229ef093
andy <andyalam@hotmail.com>
2017-02-11 17:14:03 -0800

    working on login form

---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { Card, CardSection, Input, Button } from './common';
+import { emailChanged, passwordChanged } from '../actions';
+
+
+class LoginForm extends Component {
+  onEmailChange = (text) => {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange = (password) => {
+    this.props.passwordChanged(password);
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+            onChangeText={this.onEmailChange}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry={true}
+            label="Password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange}
+            value={this.props.password}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password
+  };
+}
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
