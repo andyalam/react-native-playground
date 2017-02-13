0041273374e21da0ab488a463c5eb5535b51980a
andy <andyalam@hotmail.com>
2017-02-13 08:29:49 -0800

    set up navigation, working on creating employees

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,7 +5,7 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 
 class App extends Component {
@@ -22,10 +22,10 @@ class App extends Component {
 
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-    
+
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
