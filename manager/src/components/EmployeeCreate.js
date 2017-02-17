891a32cfc4f94db0cbf9a688a6586f1cf2a0ffa0
andy <andyalam@hotmail.com>
2017-02-16 18:56:52 -0800

    finished up employee creation, listing, hooked up to firebase

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { Picker, Text } from 'react-native';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   updateName(text) {
     this.props.employeeUpdate({ prop: 'name', value: text });
   }
 
   updatePhone(phone) {
-    this.props.employeeUpdate({ props: 'phone', value: phone });
+    this.props.employeeUpdate({ prop: 'phone', value: phone });
+  }
+
+  renderPicker() {
+    const days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                    'Saturday', 'Sunday' ];
+    return (
+      [
+        <Text style={styles.pickerTextStyle}>Shift</Text>,
+        <Picker
+          selectedValue={this.props.shift}
+          onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}>
+          {days.map(day => <Picker.Item key={day} label={day} value={day} />)}
+        </Picker>
+      ]
+    );
   }
 
   render() {
-    const { name, phone, shift } = this.props;
-    console.log('name', name);
+    const { name, phone } = this.props;
 
     return (
       <Card>
@@ -32,15 +53,16 @@ class EmployeeCreate extends Component {
             label="Phone"
             placeholder="123-123-1234"
             value={phone}
+            onChangeText={this.updatePhone.bind(this)}
           />
         </CardSection>
 
-        <CardSection>
-
+        <CardSection style={{ flexDirection: 'column' }}>
+          {this.renderPicker()}
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -49,10 +71,19 @@ class EmployeeCreate extends Component {
   }
 }
 
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
+
 const mapStateToProps = ({ employeeForm }) => {
   const { name, phone, shift } = employeeForm;
 
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeCreate
+})(EmployeeCreate);
