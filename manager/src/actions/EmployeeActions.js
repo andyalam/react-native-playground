24c539b8ae5ba47af542da77f97668efed7d42b3
andy <andyalam@hotmail.com>
2017-02-19 14:44:17 -0800

    employee editing

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,17 +3,21 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
+
+// Updates specific props of redux state
+// does NOT touch firebase db at all
 export const employeeUpdate = ({ prop, value }) => {
-  console.log('update', prop, value);
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
   };
 };
 
+// Creates new employee within firebase
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
@@ -27,6 +31,24 @@ export const employeeCreate = ({ name, phone, shift }) => {
   }
 };
 
+
+// Updates pre-existing employee within firebase
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => {
+        // after update success, reset the redux form values
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+        Actions.employeeList({ type: 'reset' });
+      });
+  }
+};
+
+
+// Grabs all employees under a specific user within firebase
 export const employeesFetch = () => {
   const { currentUser } = firebase.auth();
 
